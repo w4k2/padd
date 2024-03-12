a2a464ede2717dda77bfa4a1a6a71ee9d9211120
Joana <asiakom24@vp.pl>
2024-03-12 14:03:46 +0100

    ocdd, cddd

--- test_reference.py
+++ test_reference.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from strlearn.streams import StreamGenerator
 from reference.CDDD import CentroidDistanceDriftDetector
-from reference.OCDD import 
+from reference.OCDD import OneClassDriftDetector
 
 n_chunks = 300
 n_drifts = 5
@@ -24,24 +24,31 @@ stream = StreamGenerator(n_chunks=n_chunks,
                          random_state=876)
 
 drifts_cddd = []
+drifts_ocdd = []
 
 cddd = CentroidDistanceDriftDetector()
-ocdd = one
+ocdd = OneClassDriftDetector(size = 100, dim = 40, percent = 0.85, nu=0.5)
 
 for chunk_id in range(n_chunks):
     X, _ = stream.get_chunk()
     
     cddd.process(X)
+    ocdd.process(X)
     
     if cddd._is_drift == True:
         drifts_cddd.append(chunk_id)
+        
+    if ocdd._is_drift_chunk == True:
+        drifts_ocdd.append(chunk_id)
     
    
 print(drifts_cddd)
+print(drifts_ocdd)
 
 s = 1
 fig, ax = plt.subplots(1,1,figsize=(10,5))
 ax.vlines(drifts_cddd, 0, 1, color='red', ls=':', label='cddd')
+ax.vlines(drifts_ocdd, 0, 1, color='blue', ls=':', label='ocdd')
 ax.set_xticks(real_drifts)
 
 ax.legend()
