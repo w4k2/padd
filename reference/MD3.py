d7a5ca9ca4549da8a8477096bd343989e572ea16
Joana <asiakom24@vp.pl>
2024-03-12 16:42:02 +0100

    md3 | 89%

--- reference/MD3.py
+++ reference/MD3.py
@@ -1,134 +1,36 @@
-# https://github.com/candice-fraisse/octo_workshop_drift/blob/main/drift_detector_multivariate_md3.py
-
 import numpy as np
-import pandas as pd
-from sklearn import model_selection
 from sklearn.svm import LinearSVC
 
-import drift
-
-
-def fit_linear_SVC(X_train: pd.DataFrame, y_train: pd.DataFrame):
-    """
-    Fits a linear SVC model
-    """
-    model = LinearSVC()
-    model.fit(X_train, y_train)
-    return model
-
-
-def calc_marginal_density(model, X: pd.DataFrame, y: pd.DataFrame):
-    """
-    Calculates the margin density
-    """
-    w = model.coef_
-    w = w.T
-    b = model.intercept_
-    dp = abs(np.dot(X, w) + b)
-    rho = np.round(np.array(dp < 1).sum() / X.shape[0], 7)
-    return rho
-
-
-def calc_sigma_ref(model, X_train: pd.DataFrame, y_train: pd.DataFrame) -> float:
-    """
-    Initialises the reference sigma for the MD3 algorithm
-    """
-    kfold = model_selection.KFold(n_splits=5, shuffle=True, random_state=4369)
-    cv_results = model_selection.cross_validate(model, X_train, y_train, cv=kfold, scoring=calc_marginal_density)
-    sigma_ref = cv_results['test_score'].std()
-    return sigma_ref
-
-
-def initialise_md3(data_train: pd.DataFrame, label_col: str):
-    X_train = data_train.loc[:, data_train.columns != label_col]
-    y_train = data_train[label_col]
-    model = fit_linear_SVC(X_train, y_train)
-    rho_min = calc_marginal_density(model, X_train, y_train)
-    sigma_ref = calc_sigma_ref(model, X_train, y_train)
-    return model, rho_min, sigma_ref
-
-
-def md3_detect_drift(data_train: pd.DataFrame,
-                     data_to_compare: pd.DataFrame,
-                     label_col: str):
-    """
-    Prints an alarm or a warning if a feature drift or anomaly has happened throughout a period of days
-    """
-    model, rho_min, sigma_ref = initialise_md3(data_train, label_col)
-    X_corrupted = data_to_compare.loc[:, data_to_compare.columns != label_col]
-    y_corrupted = data_to_compare[label_col]
-    rho = calc_marginal_density(model, X_corrupted, y_corrupted)
-    if abs(rho - rho_min) > (0.25 * sigma_ref):
-        print("Alarm, drift Detected")
-
-
-def md3_gradual_drift(data_train: pd.DataFrame,
-                      data_to_compare: pd.DataFrame,
-                      column_name: str,
-                      label_col: str,
-                      value_drift: float,
-                      action: str = "increase",
-                      nb_sample: int = 100,
-                      nb_days: int = 5):
-    """
-    Generates a gradual drift
-    Prints an alarm or a warning if a feature drift or anomaly has happened throughout a period of days
-    """
-    densities = []
-    days = 0
-    init_value_drift = value_drift
-    model, rho_min, sigma_ref = initialise_md3(data_train, label_col)
-    data_generated = drift.dataset_generator_yield(data=data_to_compare, nb_sample=nb_sample)
-    while days <= nb_days:
-        print("Day", days, ":")
-        dataset_sample = next(data_generated)
-        dataset_corrupted = drift.drift_generator(data_generated=dataset_sample,
-                                                  column_name=column_name,
-                                                  value_of_drift=value_drift,
-                                                  action=action)
-        X_corrupted = dataset_corrupted.loc[:, dataset_corrupted.columns != label_col]
-        y_corrupted = dataset_corrupted[label_col]
-        rho = calc_marginal_density(model, X_corrupted, y_corrupted)
-        densities.append(rho)
-        if abs(rho - rho_min) > (0.25 * sigma_ref):
-            print("Alarm, drift Detected")
-            rho_min = rho
-        days += 1
-        value_drift += init_value_drift
-
-
-def md3_seasonal_drift(data_train: pd.DataFrame,
-                       data_to_compare: pd.DataFrame,
-                       column_name: str,
-                       label_col: str,
-                       value_drift: float,
-                       frequency: int,
-                       action: str = "increase",
-                       nb_sample: int = 100,
-                       nb_days: int = 5):
-    """
-    Generates a seasonal drift
-    Prints an alarm or a warning if a feature drift or anomaly has happened throughout a period of days
-    """
-    densities = []
-    days = 0
-    model, rho_min, sigma_ref = initialise_md3(data_train, label_col)
-    data_generated = drift.dataset_generator_yield(data=data_to_compare, nb_sample=nb_sample)
-    seasonal_days = drift.generate_frequency(nb_day=nb_days, frequency=frequency)
-    while days <= nb_days:
-        print("Day", days, ":")
-        dataset_sample = next(data_generated)
-        dataset_corrupted = drift.drift_seasonal_days_only(seasonal_days=seasonal_days,
-                                                           days=days,
-                                                           data_generated=dataset_sample,
-                                                           column_name=column_name,
-                                                           value_of_drift=value_drift,
-                                                           action=action)
-        X_corrupted = dataset_corrupted.loc[:, dataset_corrupted.columns != label_col]
-        y_corrupted = dataset_corrupted[label_col]
-        rho = calc_marginal_density(model, X_corrupted, y_corrupted)
-        densities.append(rho)
-        if abs(rho - rho_min) > (0.25 * sigma_ref):
-            print("Alarm, drift Detected")
-            rho_min = rho
-        days += 1
+class MD3:
+    def __init__(self, sigma = 0.15):
+        self.sigma = sigma
+        self.first = True
+        
+    def process(self, X, y):
+        if self.first:
+            self.model = LinearSVC().fit(X, y)
+            self.first = False
+            self._is_drift = False
+                    
+            self.rho_min = np.inf
+            self.rho_max = -np.inf
+        
+        else:
+            w = self.model.coef_
+            w = w.T
+            b = self.model.intercept_
+            dp = abs(np.dot(X, w) + b)
+            self.rho = np.array(dp < 1).sum() / X.shape[0]
+            
+            if self.rho < self.rho_min:
+                self.rho_min = self.rho
+                
+            if self.rho > self.rho_max:
+                self.rho_max = self.rho
+                
+            print(self.rho_max - self.rho_min)
+            if self.rho_max - self.rho_min > self.sigma:
+                self._is_drift = True
+                self.first = True
+            else:
+                self._is_drift = False
--- test_reference.py
+++ test_reference.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from strlearn.streams import StreamGenerator
 from reference.CDDD import CentroidDistanceDriftDetector
 from reference.OCDD import OneClassDriftDetector
+from reference.MD3 import MD3
 
 n_chunks = 300
 n_drifts = 5
@@ -25,30 +26,38 @@ stream = StreamGenerator(n_chunks=n_chunks,
 
 drifts_cddd = []
 drifts_ocdd = []
+drifts_md3 = []
 
 cddd = CentroidDistanceDriftDetector()
 ocdd = OneClassDriftDetector(size = 100, dim = 40, percent = 0.85, nu=0.5)
+md3 = MD3()
 
 for chunk_id in range(n_chunks):
-    X, _ = stream.get_chunk()
+    X, y = stream.get_chunk()
     
     cddd.process(X)
     ocdd.process(X)
+    md3.process(X,y)
     
     if cddd._is_drift == True:
         drifts_cddd.append(chunk_id)
         
     if ocdd._is_drift_chunk == True:
         drifts_ocdd.append(chunk_id)
+        
+    if md3._is_drift == True:
+        drifts_md3.append(chunk_id)
     
    
 print(drifts_cddd)
 print(drifts_ocdd)
+print(drifts_md3)
 
 s = 1
 fig, ax = plt.subplots(1,1,figsize=(10,5))
 ax.vlines(drifts_cddd, 0, 1, color='red', ls=':', label='cddd')
 ax.vlines(drifts_ocdd, 0, 1, color='blue', ls=':', label='ocdd')
+ax.vlines(drifts_md3, 0, 1, color='green', ls=':', label='md3')
 ax.set_xticks(real_drifts)
 
 ax.legend()
