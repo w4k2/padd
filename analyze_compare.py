3522f4cbf5ffd89f461e13c33d0368b038af0a6a
Joana <asiakom24@vp.pl>
2024-03-19 15:52:03 +0100

    .

---
+++ analyze_compare.py
@@ -0,0 +1,77 @@
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import matplotlib
+from utils import dderror, mask_to_indexes
+
+def get_real_drfs(n_chunks, n_drifts):
+    real_drifts = np.linspace(0,n_chunks,n_drifts+1)[:-1]
+    real_drifts += (real_drifts[1]/2)
+    return real_drifts
+
+_n_chunks = 250
+_n_drifts = 10
+
+_n_features = [30, 60, 90]
+_concept_sigmoid_spacing = [5, 999]
+ 
+reps = 10
+n_methods = 4
+
+res_dets = np.load('res/exp1_comp_v2.npy') # replications, features, concept sigmoid, detectors, chunks
+res_dets_sup = np.load('res/exp1_comp_v2_sup.npy') # replications, features, concept sigmoid, detectors, chunks
+
+res_dets = np.concatenate((res_dets, res_dets_sup), axis=3)
+print(res_dets.shape) 
+
+results = np.zeros((10,3,2,7,3))
+str_names = np.zeros((3,2)).astype('object')
+
+for n_f_id, n_f in enumerate(_n_features):
+    for css_id, css in enumerate(_concept_sigmoid_spacing):
+        str_names[n_f_id, css_id] = ('F: %02d | CSS: %03d' % (n_f, css))
+        
+        for r in range(reps):
+            for method_id in range(7):
+                
+                dets = mask_to_indexes(res_dets[r, n_f_id, css_id, method_id])
+                drifts = get_real_drfs(_n_chunks, _n_drifts).astype(int)
+                errs = dderror(drifts, dets)
+                # print(errs)
+                results[r, n_f_id, css_id, method_id] = errs                
+                
+mean_results = np.mean(results, axis=0)             
+print(mean_results.shape)
+
+mean_results = mean_results.reshape(-1,7,3)
+print(mean_results.shape)
+
+str_names = str_names.reshape(-1)
+
+    
+fig, ax = plt.subplots(1, 4, figsize=(15,4), sharex=True, sharey=True)
+
+ax[0].imshow(mean_results[:,:,0], cmap='coolwarm')
+ax[0].set_title('D1 - Detection from nearest drift')
+
+ax[1].imshow(mean_results[:,:,1], cmap='coolwarm')
+ax[1].set_title('D2 - Drift from nearest detection')
+
+ax[2].imshow(mean_results[:,:,2], cmap='coolwarm')
+ax[2].set_title('R - Detections to drifts ratio')
+
+col = np.copy(mean_results).astype(float)
+for c in range(3):
+    col[:,:,c][np.isinf(col[:,:,c])]=np.nan
+    col[:,:,c][np.isnan(col[:,:,c])]=np.nanmax(col[:,:,c])
+    col[:,:,c]-=np.min(col[:,:,c])
+    col[:,:,c]/=np.max(col[:,:,c])+0.00001
+ax[3].imshow(col)
+ax[3].set_title('Combined measures')
+
+for aa in ax:
+    aa.set_xticks(np.arange(7), ['MD3', 'OC', 'CD', 'CDET', 'ADWIN', 'DDM', 'EDDM'], rotation=90)
+    aa.set_yticks(np.arange(6), str_names)
+
+plt.tight_layout()
+plt.savefig('foo.png')
